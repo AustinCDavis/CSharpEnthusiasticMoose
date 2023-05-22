931593d7dc306096e3dbf4900030bdfdd73ba9f6
Austin Davis <davis.austin1.1.c@gmail.com>
2023-05-22 14:45:37 -0400

    completed pahse 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------");
+
