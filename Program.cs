35d74517cf6ee679144a72490b72969f7a034174
Austin Davis <davis.austin1.1.c@gmail.com>
2023-05-22 15:01:21 -0400

    completed phase 5

--- Program.cs
+++ Program.cs
@@ -3,6 +3,27 @@
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void MooseSays( string message)
 {
     Console.WriteLine($@"
@@ -35,5 +56,10 @@ void MooseSays( string message)
     ");
 }
 
+// Let the moose speak!
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
+
+// As a question
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
